cdbe7916c047c85cd65fdd61ce29a2a84430780c
sohanur1005 <sohanur.sr1005@gmail.com>
2026-01-12 23:08:10 +0600

    send this

--- js/app.js
+++ js/app.js
@@ -10,6 +10,19 @@ function loadData2(){
         const url='https://jsonplaceholder.typicode.com/users'
       fetch(url)
       .then(response => response.json())
-      .then(json => console.log(json)) 
+      .then(data=> displayUsers(data)) 
 
       }
+        function displayUsers(data){
+            const ul=document.getElementById('users-list')
+            for(const user of data)
+            {
+                
+                console.log(user.name)
+
+                const li=document.createElement('li');
+                li.innerText=user.name;
+                ul.appendChild( li);
+            }
+            
+        }
