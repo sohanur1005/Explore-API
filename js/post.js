ce58f93b2dacd97bfcdaa27ba64b8a692400541e
sohanur1005 <sohanur.sr1005@gmail.com>
2026-01-13 19:14:36 +0600

    send this

--- js/post.js
+++ js/post.js
@@ -2,5 +2,26 @@ function loadPost(){
     fetch('https://jsonplaceholder.typicode.com/posts')
 
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayPosts(data))
 }
+
+/*  get the container element where we want to add the new element
+
+*/
+
+function displayPosts(posts){
+
+    const postsContainer=document.getElementById('post-container')
+    for(const post of posts)
+    {
+        const div=document.createElement('div');
+        div.innerHTML= `  
+          <h4>User-</h4>
+          <h5>Posts: title</h5>
+          <p>Post Description</p>
+        
+        `;
+    }
+}
+
+
