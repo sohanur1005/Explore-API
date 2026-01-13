282b38774f7ccb842dedc7a42da3926d7f4bb2b4
sohanur1005 <sohanur.sr1005@gmail.com>
2026-01-13 23:32:58 +0600

    send this

--- js/coments.js
+++ js/coments.js
@@ -3,3 +3,10 @@ const loadComents=()=>{
     .then(res=>res.json())
     .then(data=>console.log(data));
 }
+
+const loadComents2=async()=>{
+    const res=await  fetch('https://jsonplaceholder.typicode.com/comments');
+    const data=await res.json();
+    console.log(data)
+
+}
